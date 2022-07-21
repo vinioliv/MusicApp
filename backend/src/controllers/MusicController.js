698b6e57253e5ff2fc2b74304f56c9b70eb3fd95
Vinicius Oliveira <58372975+vinioliv@users.noreply.github.com>
2022-07-21 00:56:05 -0300

    feat: download controller

--- backend/src/controllers/MusicController.js
+++ backend/src/controllers/MusicController.js
@@ -2,18 +2,21 @@
 
 const yt = require('youtube-search-without-api-key');
 const searchService = require("../services/SearchService");
+const downloadService = require("../services/DownloadService");
 
 module.exports = {
   async search(request, response) {
-
     const { q } = request.query;
-
     const videos = await yt.search(q.toString());
-  
     const search = await searchService.execute(videos);
-
     return response.json(search)
  
+  },
+
+  async download(request, response){
+    const {id, song} = request.body;
+    const download = await downloadService.execute(id, song);
+    return response.json(download);
   }
 }
 
